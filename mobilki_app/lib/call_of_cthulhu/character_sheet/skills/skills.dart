1271ab812b816dac6075a432ac3b31a90d3e2ba2
JanNawrat <92691845+JanNawrat@users.noreply.github.com>
2023-05-18 19:39:10 +0200

    Added path for classes related to the Skills widget and dart file for the widget itself

---
+++ mobilki_app/lib/call_of_cthulhu/character_sheet/skills/skills.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Skills extends StatefulWidget {
+  const Skills({Key? key}) : super(key: key);
+
+  @override
+  State<Skills> createState() => _SkillsState();
+}
+
+class _SkillsState extends State<Skills> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
