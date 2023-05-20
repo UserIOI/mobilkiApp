60320e526cdf943817d4db26cb4691674ac78937
UserIOI <paulharaburda@gmail.com>
2023-05-20 18:51:40 +0200

    Small changes for ui dices design

--- mobilki_app/lib/dices.dart
+++ mobilki_app/lib/dices.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:three_dart/three_dart.dart';
 
 List<int> numbers = [4, 6, 8, 10, 12, 20, 100];
 
@@ -10,6 +11,25 @@ class dices extends StatefulWidget {
 class _ParentWidgetState extends State<dices> {
   double dragStartPosition = 0, dragEndPosition = 0, navBarHeight = 70;
   double deviceWidth = 0, deviceHeight = 0;
+  bool clearing = false;
+  int d4 = 0, d6 = 0, d8 = 0, d10 = 1, d12 = 0, d20 = 0, d100 = 1, wynik = 0;
+
+  void throwDices() {
+    //** Bla bla bla */
+    wynik = d4 + d6 + d8 + d10 + d12 + d20 + d100;
+  }
+
+  void clearDices() {
+    d4 = 0;
+    d6 = 0;
+    d8 = 0;
+    d10 = 0;
+    d12 = 0;
+    d20 = 0;
+    d100 = 0;
+    throwDices();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -32,15 +52,122 @@ class _ParentWidgetState extends State<dices> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              const Center(
-                child: Text('Wynik', style: TextStyle(fontSize: 40)),
-              ),
+              Center(
+                  child: Text(
+                "$wynik",
+                style: TextStyle(fontSize: 40),
+              )),
               Container(
                 color: Colors.amber,
-                height: 300.0,
+                height: 160.0,
                 width: 300.0,
-                //** Jakos rozwiazac te dodawanie kostek do rzutu
-                //** Jakies img kostek albo cos i onClick +1 kostka pod kostami jakis counter ile ich jest, podstawowe beda ze 1xD100 i 1xD10 */
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          child: Column(children: [
+                            Text("d4"),
+                            ElevatedButton(
+                              child: Text('$d4'),
+                              onPressed: () {
+                                setState(() {
+                                  d4 = d4 + 1;
+                                });
+                              },
+                            )
+                          ]),
+                        ),
+                        Container(
+                          child: Column(children: [
+                            Text("d6"),
+                            ElevatedButton(
+                              child: Text('$d6'),
+                              onPressed: () {
+                                setState(() {
+                                  d6 = d6 + 1;
+                                });
+                              },
+                            )
+                          ]),
+                        ),
+                        Container(
+                          child: Column(children: [
+                            Text("d8"),
+                            ElevatedButton(
+                              child: Text('$d8'),
+                              onPressed: () {
+                                setState(() {
+                                  d8 = d8 + 1;
+                                });
+                              },
+                            )
+                          ]),
+                        ),
+                        Container(
+                          child: Column(children: [
+                            Text("d10"),
+                            ElevatedButton(
+                              child: Text('$d10'),
+                              onPressed: () {
+                                setState(() {
+                                  d10 = d10 + 1;
+                                });
+                              },
+                            )
+                          ]),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          child: Column(children: [
+                            Text("d12"),
+                            ElevatedButton(
+                              child: Text('$d12'),
+                              onPressed: () {
+                                setState(() {
+                                  d12 = d12 + 1;
+                                });
+                              },
+                            )
+                          ]),
+                        ),
+                        Container(
+                          child: Column(children: [
+                            Text("d20"),
+                            ElevatedButton(
+                              child: Text('$d20'),
+                              onPressed: () {
+                                setState(() {
+                                  d20 = d20 + 1;
+                                });
+                              },
+                            )
+                          ]),
+                        ),
+                        Container(
+                          child: Column(children: [
+                            Text("d100"),
+                            ElevatedButton(
+                              child: Text('$d100'),
+                              onPressed: () {
+                                setState(() {
+                                  d100 = d100 + 1;
+                                });
+                              },
+                            )
+                          ]),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                //** Dodac unity ???
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -48,13 +175,17 @@ class _ParentWidgetState extends State<dices> {
                   //** Rozmiar buttonów oraz wyrzej dać widgety */
                   ElevatedButton(
                     onPressed: () {
-                      Navigator.pop(context);
+                      setState(() {
+                        clearDices();
+                      });
                     },
                     child: const Text('CLEAR'),
                   ),
                   ElevatedButton(
                     onPressed: () {
-                      Navigator.pop(context);
+                      setState(() {
+                        throwDices();
+                      });
                     },
                     child: const Text('THROW'),
                   ),
